d32e02713259e1e683cf2d3457a07a56d0b92b92
calexconc <calujo.conceicao@gmail.com>
2012-03-26 21:00:27 +0100

    Add Test Project and dependencies

---
+++ UnitTests/ConfigTests.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Calex
+ * Date: 22-03-2012
+ * Time: 14:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class ConfigTests
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			// TODO: Add Init code.
+		}
+		
+		[TestFixtureTearDown]
+		public void Dispose()
+		{
+			// TODO: Add tear down code.
+		}
+	}
+}
---
+++ UnitTests/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Calex
+ * Date: 22-03-2012
+ * Time: 14:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.ConsoleRunner;
+using System.Reflection;
+
+namespace UnitTests
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string[] my_args = { Assembly.GetExecutingAssembly().Location };
+			
+			int returnCode = Runner.Main(my_args);
+
+            if (returnCode != 0)
+                Console.Beep();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
