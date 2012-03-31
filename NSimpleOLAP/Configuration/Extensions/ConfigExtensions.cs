7e270edb7990a1cade17e279d5e0d2a8f5c3672e
calexconc <calujo.conceicao@gmail.com>
2012-03-31 02:21:47 +0100

    Fluent Interface Correction

--- NSimpleOLAP/Configuration/Extensions/ConfigExtensions.cs
+++ NSimpleOLAP/Configuration/Extensions/ConfigExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using NSimpleOLAP;
 using NSimpleOLAP.Configuration;
-using NSimpleOLAP.Configuration.Fluent;
+
 
 namespace NSimpleOLAP.Configuration.Extensions
 {
@@ -22,21 +22,5 @@ namespace NSimpleOLAP.Configuration.Extensions
 			return dict;
 		}
 		
-		public static CubeBuilder SetupConfig<T>(this Cube<T> cube)
-			where T: struct, IComparable
-		{
-			cube.Config = new CubeConfig();
-			CubeBuilder builder = new CubeBuilder(cube.Config);
-				
-			return builder;
-		}
-		
-		public static Cube<T> Create<T>(this CubeBuilder cubebuilder)
-			where T: struct, IComparable
-		{
-			CubeConfig cube = cubebuilder.CreateConfig();
-		
-			return new Cube<T>(cube);
-		}
 	}
 }
---
+++ NSimpleOLAP/Configuration/Fluent/FluentExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using NSimpleOLAP;
+using NSimpleOLAP.Configuration;
+
+namespace NSimpleOLAP.Configuration.Fluent
+{
+	/// <summary>
+	/// Description of FluentExtensions.
+	/// </summary>
+	public static class FluentExtensions
+	{
+		public static CubeBuilder SetupConfig<T>(this Cube<T> cube)
+			where T: struct, IComparable
+		{
+			cube.Config = new CubeConfig();
+			CubeBuilder builder = new CubeBuilder(cube.Config);
+				
+			return builder;
+		}
+		
+		public static Cube<T> Create<T>(this CubeBuilder cubebuilder)
+			where T: struct, IComparable
+		{
+			CubeConfig cube = cubebuilder.CreateConfig();
+		
+			return new Cube<T>(cube);
+		}
+	}
+}
